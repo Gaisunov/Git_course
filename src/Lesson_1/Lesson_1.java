8368dd00cd0c2af79c1a4d047caa2b3320decebd
Ibragim <Gaisunov.i.b@inbox.ru>
2023-01-30 12:38:29 +0300

    Add file 'Lesson_1.java' in directory 'lesson_1'.

---
+++ src/Lesson_1/Lesson_1.java
@@ -0,0 +1,11 @@
+package Lesson_1;
+
+public class Lesson_1 {
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 0; i < 1000; i++) {
+            count++;
+            System.out.println("Count=" + count);
+        }
+    }
+}
